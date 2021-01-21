f52a95fee2a7e6b777363305b08fbafa4744341e
jiangnanhuabei <13343257639@163.com>
2021-01-21 23:47:13 +0800

    '初始化'

---
+++ src/main/java/com/jiang/MallTryApplication.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTryApplication.class, args);
+    }
+
+}
